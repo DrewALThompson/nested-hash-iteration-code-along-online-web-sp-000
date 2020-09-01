e157ca071bcd80ac8a53ea1bbecda791717a0d4f
Drew Thompson <drewallynthompson@gmail.com>
2020-09-01 02:37:13 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,6 +1,5 @@
 require 'pry'
 
-  This is the array we will be passing into the remove_strawberry method
   contacts = {
     "Jon Snow" => {
       name: "Jon",
@@ -15,5 +14,6 @@ require 'pry'
   }
   
 def remove_strawberry(contacts)
-
+contacts.each do |key, value|
+  
 end
